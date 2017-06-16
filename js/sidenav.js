6497e500fceda24764a3132a370478925a1fcf5f
James Wall <james.wall@ucf.edu>
2017-06-16 15:59:23 -0400

    WAH-38 Sidebar now highlights and code was made more versatile.
    Signed-off-by: James Wall <james.wall@ucf.edu>

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,86 +1,36 @@
 //This script causes the extended team sidebar to highlight on scroll.
 
 $(document).scroll(function() {
+	// Get current scroll position
 	var scroll_top = $(document).scrollTop() + 51;
-	var team_1_top = $('#Team1').position().top;
-	var team_2_top = $('#Team2').position().top;
-	var team_3_top = $('#Team3').position().top;
-	var team_4_top = $('#Team4').position().top;
-	var team_5_top = $('#Team5').position().top;
-	var team_6_top = $('#Team6').position().top;
-	var team_7_top = $('#Team7').position().top;
-	var team_8_top = $('#Team8').position().top;
-	var team_9_top = $('#Team9').position().top;
-	var team_10_top = $('#Team10').position().top;
-	var team_11_top = $('#Team11').position().top;
-	var team_12_top = $('#Team12').position().top;
-	var team_13_top = $('#Team13').position().top;
 	
-	if(scroll_top < team_1_top){
-		$('#sidenav li a').removeClass('active');
+	// Count the number of items in side nav
+	var count = document.getElementById("sidenav").childElementCount;
+	
+	// Fill array with the position of each element
+	var scrollPoint = [];
+	for(var i=0;i<count;i++){
+		scrollPoint[i] = $('#Team' + (i+1)).position().top;
 	}
-	else if(scroll_top > team_1_top && scroll_top < team_2_top) {
-		//You are now on team 1
-		$('#sidenav li a').removeClass('active');
-		$('#nav1').addClass('active');
-	} else if( scroll_top > team_2_top && scroll_top < team_3_top) {
-		//You are now on team 2
-		$('#sidenav li a').removeClass('active');
-		$('#nav2').addClass('active');
+	
+	// Compare current position with table positions
+	// First if checks if the current pos is above all tables
+	if(scroll_top < scrollPoint[0]){
+		$('#sidenav li a').removeClass('active');	// Removes all if scrolled to top
 	}
-	else if( scroll_top > team_3_top && scroll_top < team_4_top) {
-		//You are now on team 3
-		$('#sidenav li a').removeClass('active');
-		$('#nav3').addClass('active');
+	// Second if checks tables 1 through n-1
+	else if(scroll_top >= scrollPoint[0] && scroll_top < scrollPoint[count-1]){
+		for(var i = 0;i<count;i++){
+			if(scroll_top > scrollPoint[i] && scroll_top < scrollPoint[i+1]){
+				$('#sidenav li a').removeClass('active');	// Removes from all other items
+				$('#nav' + (i+1)).addClass('active');	// Adds active to element
+				break;
+			}
+		}
 	}
-	else if( scroll_top > team_4_top && scroll_top < team_5_top) {
-		//You are now on team 4
-		$('#sidenav li a').removeClass('active');
-		$('#nav4').addClass('active');
-	}
-	else if( scroll_top > team_5_top && scroll_top < team_6_top) {
-		//You are now on team 5
-		$('#sidenav li a').removeClass('active');
-		$('#nav5').addClass('active');
-	}
-	else if( scroll_top > team_6_top && scroll_top < team_7_top) {
-		//You are now on team 6
-		$('#sidenav li a').removeClass('active');
-		$('#nav6').addClass('active');
-	}
-	else if( scroll_top > team_7_top && scroll_top < team_8_top) {
-		//You are now on team 7
-		$('#sidenav li a').removeClass('active');
-		$('#nav7').addClass('active');;
-	}
-	else if( scroll_top > team_8_top && scroll_top < team_9_top) {
-		//You are now on team 8
-		$('#sidenav li a').removeClass('active');
-		$('#nav8').addClass('active');
-	}
-	else if( scroll_top > team_9_top && scroll_top < team_10_top) {
-		//You are now on team 9
-		$('#sidenav li a').removeClass('active');
-		$('#nav9').addClass('active');
-	}
-	else if( scroll_top > team_10_top && scroll_top < team_11_top) {
-		//You are now on team 10
-		$('#sidenav li a').removeClass('active');;
-		$('#nav10').addClass('active');
-	}
-	else if( scroll_top > team_11_top && scroll_top < team_12_top) {
-		//You are now on team 11
-		$('#sidenav li a').removeClass('active');
-		$('#nav11').addClass('active');
-	}
-	else if( scroll_top > team_12_top && scroll_top < team_13_top) {
-		//You are now on div team 12
-		$('#sidenav li a').removeClass('active');
-		$('#nav12').addClass('active');
-	}
-	else if( scroll_top > team_13_top) {
-		//You are now on div team 13
-		$('#sidenav li a').removeClass('active');
-		$('#nav13').addClass('active');
+	// Third if highlights the item n for the last table
+	else if(scroll_top >= scrollPoint[count-1]) {
+		$('#sidenav li a').removeClass('active');	// Removes from all other items
+		$('#nav' + (count)).addClass('active');	// Adds active to element
 	}
 });
